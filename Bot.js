dec840b35a613a27774e91cd19dbc849a42a9e38
Luigi Faticoso <luxor320@gmail.com>
2016-01-29 20:23:26 +0100

    Bot.js
    Aggiunta la funzione Menù

--- Bot.js
+++ Bot.js
@@ -10,32 +10,46 @@ var authorized_users = [
 var tgbot = require('node-telegram-bot-api');
 
 // inizializziamo i relay/led tramite la libreria onoff
-//var Gpio = require('onoff').Gpio, //libreria onoff
+//libreria onoff
+//var Gpio = require('onoff').Gpio, 
 //  led1 = new Gpio(2, 'out'), //led settato come output
 //  relay1 = new Gpio(3, 'out');
 
-// Spegniamo i relay/led ad ogni avvio del server da rasp
+// Spegniamo i relay/led ad ogni avvio del bot da rasp
 //led1.writeSync(0);
 //relay1.writeSync(0);
 
 
 // Inizializzazione del bot con il token
-var token = "147183253:AAGkGQs690kUAdBaSfUPBgg5ayHJihJh_s0";
+var token = "YOUR BOT TOKEN";
 var bot = new tgbot(token, {polling:true});
 
-bot.onText(/\/torvy/, function (msg) { //quando riceve il comando /torvy
+//se attivato il terminale legge tutti i messaggi inviati al bot
+/* bot.onText('message', function (msg)
+{
+    console.log(message);
+}); */
+
+
+ //quando riceve il comando /torvy
+ //bot.onText(/\/torvy/, function (msg) {
+bot.onText('TORVY', function (msg) { //da testare il funzionamento del riconoscimento del messaggio "TORVY"
         if(!isAuthorized(msg.from.id)) return; //controlla che sia l'id autorizzato
   var chatId = msg.chat.id;
   var opts = {
       reply_to_message_id: msg.message_id, //ti quota il messaggio che hai inviato
       reply_markup: JSON.stringify({
         keyboard: [
-          ['ON','OFF']],
+          ['ON','OFF']
+          ['MENU']],
         one_time_keyboard:true,  //dopo che la clicchi scompare
         resize_keyboard:true     //ridimensiona i bottoni
       })
     };
     bot.sendMessage(chatId, 'ACCESO O SPENTO?', opts);
+    bot.onText('MENU', function(msg){    //da testare, come la riga 36
+      menu(chatId);
+    }
 });
 
 // Controlla se l'id è autorizzato
@@ -45,4 +59,21 @@ function isAuthorized(userid) {
     if(authorized_users[i ] == userid) return true;
 
   return false;
+}
+
+void function Menu(cID){
+  var kmenu = {
+      //reply_to_message_id: msg.message_id, //ti quota il messaggio che hai inviato
+      reply_markup: JSON.stringify({
+        keyboard: [
+          ['TORVY']
+          ['SMOKY','MOVY']
+          ['HELP','SETTINGS']],
+        one_time_keyboard:true,  //dopo che la clicchi scompare
+        resize_keyboard:true     //ridimensiona i bottoni
+      })
+    };
+    bot.sendMessage(cID, 'Serve altro?', kmenu);
+}
+  
 }
